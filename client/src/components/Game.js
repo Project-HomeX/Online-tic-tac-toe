1cf417084b16343089d3cbd08c52dd612aa48797
gutama8787 <gutama8787@gmail.com>
2020-07-20 14:18:05 -0400

    P5 implimentation done

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,39 +1,18 @@
 //This is supposed to represent the game from one client side.
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Sketch from 'react-p5';
+import Tic from './Tic';
 import Board from './Board';
 import { NavLink } from "react-router-dom";
 class Game extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            matrix: [[0 , 0 , 0], [0, 0 , 0], [0, 0, 0]],
-            flag: true,
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
 
-    handleClick(e){
-        let u = e.target.name.split(",");
-        let x = parseInt(u[0]);
-        let y = parseInt(u[1]);
-        if(this.state.matrix[x][y] == 0){
-            console.log("x: " + x + " y: " + y);
-            if(this.state.flag){
-                this.state.matrix[x][y] = 1;
-            }else{
-                this.state.matrix[x][y] = -1;
-            }
-        }
-        
-    }
-
-    render(){
-        return (
-            <div>
-                <Board matrix = {this.state.matrix} handler = {this.handleClick}/>
-            </div>
-        )
-    }
-}
-            
+	render() {
+		return (
+			<div>
+				<Tic/>
+			</div>
+		);
+	}
+}            
 export default Game;
