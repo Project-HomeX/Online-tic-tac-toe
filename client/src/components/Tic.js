dbc127060c58a203b32bcd43f95bc39b888d70a9
ZAbrham <abrhamzemene16@gmail.com>
2020-07-30 08:15:30 -0400

    before merge

--- client/src/components/Tic.js
+++ client/src/components/Tic.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
 // import logo from './logo.svg';
@@ -6,26 +5,40 @@ import socketIOClient from "socket.io-client";
 
 import Sketch from "react-p5";
 let sum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let matrix = [[0, 0, 0],
-[0, 0, 0],
-[0, 0, 0]];
+let matrix = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
 let flag = true;
-let turn = true;
+let color = 'blue'
 let x;
 let y;
 let win;
+
 function Tic(props) {
   let ENDPOINT = "http://127.0.0.1:4000";
   let socket;
   let globalP5;
+  let turn = true;
+  // let [turn, setTurn] = useState(true)
+
+  // let [color, setColor] = useState('red');
+  
   useEffect(() => {
-    socket = socketIOClient(ENDPOINT);
-    socket.on("updateMatrix", ({ x, y , flag}) => {
-      turn = flag;
-      colorBoxes(globalP5, x, y);
+   socket = socketIOClient(ENDPOINT);
+   /* socket.on('changeTurn', () => {
+      console.log("Changing turn to true")
+      // setTurn(true)
+      turn = true;
+    })*/
+    socket.on("updateMatrix", ({tempVal, x, y,color}) => {
+      matrix[x][y] = tempVal;
+     
+      console.log('after matrix update: '+turn + ' '+color)
+      colorBoxes(globalP5, x, y,color);
+      turn = true;
+      //color = 'red';
       console.log("Recived values\n X: " + x + " Y: " + y);
     });
-  }, []);
+  }, [ENDPOINT]);
+  
   // const [sum, setSum] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   //const [matrix, setMatrix] = useState([[0, 0, 0],[0, 0, 0],[0, 0, 0]]);
   //const [flag, setFlag] = useState(true);
@@ -60,21 +73,35 @@ function Tic(props) {
     p5.line(200, 0, 200, 600);
     p5.line(400, 0, 400, 600);
   }
-  function colorBoxes(p5, x, y) {
-    let color = (flag) ? "blue" : "red";
-    p5.fill(color);
+  function colorBoxes(p5, x, y,c) {
+    console.log(c);
+    // let color = (flag) ? "blue" : "red";
+    p5.fill(c);
     p5.rect(x * 200, y * 200, 200, 200);
+    
+    // change
+    
+    if(c === 'red') {
+      color = 'blue';
+    }
+    if(c === 'blue'){
+      color = 'red';
+    }
+    
+    console.log(color)
+    console.log(matrix)
   }
   const mouseClicked = (p5) => {
-    if (true) {
+    console.log(turn)
+    if (turn) {
+      // setTurn(false)
       x = p5.mouseX;
       y = p5.mouseY;
       if (x < 600 && y < 600 && !win && x > 0 && y > 0) {
         let px = p5.floor(x / 200);
         let py = p5.floor(y / 200);
         let tempVal = -1;
-        if (matrix[px][py] === 0) {
-          socket.emit("sendNewMove", ({ x: py, y: py }));
+        if (matrix[px][py] === 0) { 
           if (flag) {
             tempVal = -1;
           } else {
@@ -82,13 +109,15 @@ function Tic(props) {
           }
           flag = !flag;
           matrix[px][py] = tempVal;
-          colorBoxes(p5, px, py)
+          socket.emit("sendNewMove", ({tempVal, x: px, y: py, color,turn}));
+          turn = false;
+          //color = 'blue';
+          colorBoxes(p5, px, py,color)
           addToSum(px, py, tempVal);
           logic(p5)
         }
       }
     }
-
   };
 
   //[v1,v2,v3,h1,h2,h3,d1,d1]
@@ -158,4 +187,180 @@ function Tic(props) {
     padding: "10px",
   }} setup={setup} draw={draw} mouseClicked={mouseClicked} />);
 };
+
+
 export default Tic;
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import socketIOClient from "socket.io-client";
+// import Sketch from "react-p5";
+// let sum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+// let matrix = [[0, 0, 0],
+// [0, 0, 0],
+// [0, 0, 0]];
+// let flag = true;
+// let turn = true;
+// let color = "blue"
+// let x;
+// let y;
+// let win;
+// let tempVal = 1;
+// function Tic(props) {
+//   let ENDPOINT = "http://127.0.0.1:4000";
+//   let socket;
+//   let globalP5;
+//   useEffect(() => {
+//     //socket connecting to server
+//     socket = socketIOClient(ENDPOINT);
+//     //updating this player's board based on the other player
+//     socket.on("updateMatrix", ({ x, y}) => {
+//       color = color == "blue" ? "red" : "blue"
+//       temp(globalP5, x, y);
+//       colorBoxes(globalP5, x, y)
+//       console.log("Recived values\n X: " + x + " Y: " + y);
+//     });
+//     socket.on("updateWinStatus", () => {
+//       win = true;
+//     })
+//     socket.on("changeTurn", () => {
+//       console.log("Changing turns!")
+//       tempVal = 1;
+//       turn = true;
+//     })
+//   }, []);
+//   // const [sum, setSum] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+//   //const [matrix, setMatrix] = useState([[0, 0, 0],[0, 0, 0],[0, 0, 0]]);
+//   //const [flag, setFlag] = useState(true);
+//   const setup = (p5, canvasParentRef) => {
+//     // use parent to render the canvas in this ref
+//     // (without that p5 will render the canvas outside of your component)
+//     p5.createCanvas(600, 600).parent(canvasParentRef);
+//     update(p5);
+//     globalP5 = p5;
+//   };
+//   const draw = (p5) => {
+//     if (props.isClicked) {
+//       update(p5);
+//       props.falseIsClicked();
+//     }
+//   }
+//   function update(p5) {
+//     sum = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+//     matrix = [[0, 0, 0],
+//     [0, 0, 0],
+//     [0, 0, 0]];
+//     flag = true;
+//     win = false;
+//     p5.background(0);
+//     p5.stroke(255);
+//     p5.strokeWeight(10);
+//     // horizontal 
+//     p5.line(0, 200, 600, 200);
+//     p5.line(0, 400, 600, 400);
+
+//     // vertical 
+//     p5.line(200, 0, 200, 600);
+//     p5.line(400, 0, 400, 600);
+//   }
+//   function colorBoxes(p5, x, y) {
+//     console.log(matrix)
+//     p5.fill(color);
+//     p5.rect(x * 200, y * 200, 200, 200);
+//   }
+//   function temp(p5, x, y, fromHere) {
+//     if (true) {
+//       if (x < 600 && y < 600 && !win && x > 0 && y > 0) {
+//         let px = p5.floor(x / 200);
+//         let py = p5.floor(y / 200);
+//         //let tempVal = -1;
+//         if (matrix[px][py] === 0) {
+//           if(turn) socket.emit("sendNewMove", ({ x: px, y: py }));
+//           console.log("sending values\nx: " + px + " y: " + py)
+//           color = color == "blue" ? "red" : "blue"
+//           turn = false;
+//           tempVal = -1;
+//           flag = !flag;
+//           matrix[px][py] = tempVal;
+//           colorBoxes(p5, px, py)
+//           addToSum(px, py, tempVal);
+//           logic(p5)
+//         }
+//       }
+//     }
+//   }
+//   const mouseClicked = (p5) => {
+//     if (turn) {
+//       x = p5.mouseX;
+//       y = p5.mouseY;
+//       temp(p5, x, y, true);
+//     }
+//   };
+
+//   //[v1,v2,v3,h1,h2,h3,d1,d1]
+//   function addToSum(x, y, v) {
+//     if (x === 0 && y === 0) {
+//       sum[0] += v;
+//       sum[3] += v;
+//       sum[7] += v;
+//     } else if (x === 0 && y === 1) {
+//       sum[0] += v;
+//       sum[4] += v;
+//     } else if (x === 0 && y === 2) {
+//       sum[0] += v;
+//       sum[5] += v;
+//       sum[6] += v;
+//     } else if (x === 1 && y === 0) {
+//       sum[1] += v;
+//       sum[3] += v;
+//     } else if (x === 1 && y === 1) {
+//       sum[1] += v;
+//       sum[4] += v;
+//       sum[6] += v;
+//       sum[7] += v;
+//     } else if (x === 1 && y === 2) {
+//       sum[1] += v;
+//       sum[5] += v;
+//     } else if (x === 2 && y === 0) {
+//       sum[2] += v;
+//       sum[3] += v;
+//       sum[6] += v;
+//     } else if (x === 2 && y === 1) {
+//       sum[2] += v;
+//       sum[4] += v;
+//     } else if (x === 2 && y === 2) {
+//       sum[2] += v;
+//       sum[5] += v;
+//       sum[7] += v;
+//     }
+//   }
+
+//   function logic(p5) {
+//     for (let x = 0; x < 8; x++) {
+//       if (sum[x] === -3 || sum[x] === 3) {
+//         if (sum[x] == -3) {
+//           props.updateScore(1, 0);
+//         } else {
+//           props.updateScore(0, 1);
+//         }
+//         socket.emit("win");
+//         win = true;
+//       }
+//     }
+//   }
+
+//   return (<Sketch style={{
+//     display: "flex",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     padding: "10px",
+//   }} setup={setup} draw={draw} mouseClicked={mouseClicked} />);
+// };
+// export default Tic;
--- server/app.js
+++ server/app.js
@@ -1,33 +1,93 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
+ 
 const PORT = process.env.PORT || 4000;
 const index = require("./index");
-
+ 
 const app = express();
 app.use(index);
-
+ 
 const server = http.createServer(app);
-
+ 
+const users = [];
+ 
 const io = socketIo(server);
 let turn = true;
 let numClients = 0;
 io.on('connect', socket => {
-    numClients ++; 
-    
-    console.log("Client has Joined");
-    //io.broadcast('setUp', turn);
-    socket.on("sendNewMove", ({x,y}) => {
-        console.log("Server recived the following values\nx: " + x + " y: " + y)
-        turn != turn;
-        io.emit("updateMatrix", {x, y, turn});
-    })
+    numClients++;
+    // if(users.length<2)
+        users.push(socket);
+    if (users.length >= 2) {
+       // let otherSocket = users.find(user => user.id !== socket.id);
+       // console.log("other: "+ otherSocket.id + " this:" + socket.id);
+        console.log("Client has Joined");
+        //io.broadcast('setUp', turn);
+        socket.on("sendNewMove", ({ tempVal,x, y,color,turn}) => {
+            otherSocket = users.find(user => user.id !== socket.id);
+            console.log("Server recived the following values\nx: " + x + " y: " + y)
+            // otherSocket.emit('changeTurn');
+            console.log("turn: "+turn)
+            //socket.broadcast.emit("updateMatrix", { tempVal,x, y,color});
+            socket.broadcast.emit("updateMatrix", { tempVal,x, y,color,turn});
+ 
+        })
+ 
+    }
     socket.on("disconnect", () => {
-        numClients --;
-        console.log(numClients,"client has left")
+        numClients--;
+        console.log(numClients, "client has left")
     })
     console.log(numClients, socket.id)
 })
-
+ 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+
+
+
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+
+// const PORT = process.env.PORT || 4000;
+// const index = require("./index");
+
+// const app = express();
+// app.use(index);
+
+// const server = http.createServer(app);
+
+// const io = socketIo(server);
+// let numClients = 0;
+// io.on('connect', socket => {
+//     numClients ++; 
+//     socket.join("room");
+//     // if(numClients == 1){
+//     //     socket.emit("color", "blue");
+//     // }else{
+//     //     socket.emit("color", "red"); 
+//     // }
+//     console.log("Client has Joined");
+    
+//     socket.on("sendNewMove", ({x,y}) => {
+//         console.log("Server recived the following values\nx: " + x + " y: " + y)
+//         socket.broadcast.to("room").emit("updateMatrix", {x, y});
+//         socket.broadcast.to("room").emit('changeTurn');
+//     })
+//     socket.on("disconnect", () => { 
+//         numClients --;
+//         console.log(numClients,"client has left")
+//     })
+
+//     socket.on("win", () => {
+//         io.emit("updateWinStatus");
+//     })
+
+    
+//     console.log(numClients, socket.id)
+// })
+
+// server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
